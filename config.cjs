8f3ea650f3b8ee6f380bb78925a68bc205be3668
6Mirza <uj007hsj@gmail.com>
2025-06-10 10:12:53 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,15 +3,15 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  SESSION_ID: process.env.SESSION_ID || "Sarkarmd$eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYU9hZzh6NzRjVmduakxtRTBKdGFMSXpxVG1RSjkxcWlmSFpGN1hrbXowTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMnJtb1N3ekU5YjgzVjk3MlNTNHY4bkpKdm11dVJZR3lTbjZUbFVwaXFuWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJvQmRIYXdvV3RTOVZBbC9sVmVmekh5Q1hqL3ZZdC93cTlOWW4wb2c4MEhBPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJUQkptYTNxaUlLV1cxZ3ZlOEcvMnNUV3F0Q2JtblRkWVlINkZXcHJudUhjPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkdHNURPN3FYekx5NVA3am1naW5LQmJWUGZ0KzhBU3RPcHN1K1BNMmVoVWM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjQ3SUxWdmdXelR6VnVFamkrdmNVcGlDWFFac3NteWdqRHpEUitYdDM5eVU9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoid0xsbkJaVFFkK0pqbDE2eXBaeVU2c21ZTFpxUWd0eStva1c5U0RyNzZHRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia3U2SUhmSllYMGZpL1VucmNGUDZ2RjAwU3g4bFFSeTdvalZSZ0g3djB6Zz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImxiYTQxQW9GV3lGZkdWRkFaL3BEQ0pjdW9DU1laMVBNczR4d0llYWhmTzFzM3RYZmMyRXRjaHhrY2lCUDZ6Q2hDeXY2MzMrc0YrMDhaWklXOEk1RWpnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTg4LCJhZHZTZWNyZXRLZXkiOiJNcklFWVl4NzQ3UUR5YXUzU2pqZzhIMTlzbXgxVmN6VURFelVBSWd0V3J3PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwicmVnaXN0ZX",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
-  AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
+  AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : true,
   AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
-  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
-  AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== true  process.env.ALWAYS_ONLINE === 'true' : false,
+  AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : true,
    /*auto block only for 212 */
   AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
   
